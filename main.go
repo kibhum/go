f9f30d250c76d82f8f22a6d6a618db86b6d13e60
kibhum <kibethh@gmail.com>
2024-07-26 00:30:46 +0300

    Explored variables

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var name = "Kibet"
+const firstName="Humphrey"
+
+var (
+	occupation = "developer"
+	age        int
+	language   = "golang"
+)
+
+const (
+	mentor = "HHH"
+	show = "WWE"
+)
+
+func main() {
+	country := "Kenya"
+
+	fmt.Println(country)
+	fmt.Println(firstName)
+	fmt.Println(mentor)
+}
